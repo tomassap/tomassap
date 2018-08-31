e242cc5ca5c255de8d017567f3533281ff54892c
tomassap <33234407+tomassap@users.noreply.github.com>
2018-08-31 12:27:15 -0400

    logout functionality

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -17,6 +17,12 @@ class Login extends Component {
     this.onSubmit = this.onSubmit;
   }
 
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.auth.isAuthenticated) {
       this.props.history.push("/dashboard");
