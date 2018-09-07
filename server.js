229dc8091318ff293695e037e9a53e145db24701
tomassap <33234407+tomassap@users.noreply.github.com>
2018-09-07 17:28:33 -0400

    static

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use("/api/emails", emails);
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // set static folder
-  app.use("/static", express.static(path.join(__dirname, "client/build")));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
